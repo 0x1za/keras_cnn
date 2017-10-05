5a8adfc9205aa3aa8fa64d5a40d478ec8bd1f589
Mwiza Simbeye <mwizasimbeye@gmail.com>
2017-10-05 12:32:47 +0200

    No favors

--- cnn.py
+++ cnn.py
@@ -3,6 +3,7 @@ from keras.layers import MaxPool2D, Conv2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 # dimensions of our images.
 img_width, img_height = 150, 150
@@ -31,3 +32,35 @@ model.add(MaxPool2D(pool_size=(2, 2)))
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPool2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',optimizer='rmsprop', metrics=['accuracy'])
+
+batch_size = 16
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    'train',
+    target_size=(150, 150),
+    batch_size=batch_size,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    'validation',
+    target_size=(150, 150),
+    batch_size=batch_size,
+    class_mode='binary')
+
