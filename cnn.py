342bbec023819c4665e806f71b30443b12d79226
Mwiza Simbeye <mwizasimbeye@gmail.com>
2017-10-05 23:28:24 +0200

    Working Version

--- cnn.py
+++ cnn.py
@@ -1,15 +1,16 @@
+
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import MaxPool2D, Conv2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
 
 # dimensions of our images.
 img_width, img_height = 150, 150
 
-train_data_dir = 'data/train'
-validation_data_dir = 'data/validation'
+train_data_dir = 'train'
+validation_data_dir = 'validation'
 nb_train_samples = 2000
 nb_validation_samples = 800
 epochs = 50
@@ -21,17 +22,17 @@ else:
     input_shape = (img_width, img_height, 3)
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(3, 150, 150)))
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 model.add(Activation('relu'))
-model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
-model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
-model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
 model.add(Dense(64))
@@ -40,29 +41,38 @@ model.add(Dropout(0.5))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
-model.compile(loss='binary_crossentropy',optimizer='rmsprop', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
 
-batch_size = 16
-
+# this is the augmentation configuration we will use for training
 train_datagen = ImageDataGenerator(
-    rescale=1./255,
+    rescale=1. / 255,
     shear_range=0.2,
     zoom_range=0.2,
     horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
-    'train',
-    target_size=(150, 150),
+    train_data_dir,
+    target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
-    'validation',
-    target_size=(150, 150),
+    validation_data_dir,
+    target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='binary')
 
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
 
-
+model.save_weights('first_try.h5')
